3d89cbe3bb2dd87a8bce12d006f47e4d68384fbc
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-14 12:14:41 +0200

    :)

---
+++ Böcker 3/Böcker 3/Books.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Böcker_3
+{
+    class Product
+    {
+        public string ProductId { get; set; }
+        //private int _id;
+
+
+        //public int GetProductId()
+        //{
+        //    return _id;
+        //}
+
+        //public void SetProductId(int id)
+        //{
+        //    _id = id;
+
+        //}
+
+    }
+
+
+
+    class ElectronicBook : Book
+    {
+        public ElectronicBook(string isbn, string title) : base(isbn, title)
+        {
+
+        }
+
+        public void SendBookTo(string emailAdress)
+        {
+            Console.WriteLine("Send book to " + emailAdress);
+        }
+    }
+
+
+
+
+    class Book : Product
+    {
+        //private string _isbn;
+        //private string _author;
+        //private double _number;
+        //private double _title;
+
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public double NrOfPages { get; set; }
+        public string Title { get; set; }
+
+        public Book(string isbn, string title)
+        {
+            Isbn = isbn;
+            Title = title;
+        }
+
+
+        //public string GetIsbn()
+        //{
+        //    return _isbn;
+        //}
+
+        //public void SetIsbn(string isbn)
+        //{
+        //    _isbn = isbn;
+
+        //}
+
+        //public string GetAuthor()
+        //{
+        //    return _author;
+        //}
+
+        //public void SetAuthor(string author)
+        //{
+        //    _author = author;
+
+        //}
+
+
+        //public double GetNrOfPages()
+        //{
+        //    return _number;
+        //}
+
+        //public void SetNrOfPages(double number)
+        //{
+        //    _number = number;
+
+        //}
+
+
+        public double WeightInGram(double number)
+        {
+            return number * 0.8;
+
+        }
+
+        public string Size(double number)
+        {
+            if (number < 100)
+            {
+                return "Tunn";
+            }
+            else if (number > 100 && number < 299)
+            {
+                return "Normal";
+            }
+            else
+                return "Tjock";
+        }
+    }
+}
+
---
+++ Böcker 3/Böcker 3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Böcker_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Book("111111", "Min bok");
+
+            b1.Isbn="978-3-16-148410-0";
+            b1.Author="Kalle";
+            b1.NrOfPages=400;
+            b1.ProductId="1234";
+
+            Console.WriteLine($"Info om boken:");
+            Console.WriteLine();
+            Console.WriteLine($"   ISBN:        {b1.Isbn}");
+            Console.WriteLine($"   Författare:  {b1.Author}");
+            Console.WriteLine($"   Antal sidor: {b1.NrOfPages}");
+            Console.WriteLine($"   Vikt:        {b1.WeightInGram(b1.NrOfPages)} gram");
+            Console.WriteLine($"   Storlek:     {b1.Size(b1.NrOfPages)}");
+            Console.WriteLine($"   ProductId:   {b1.ProductId}");
+            Console.WriteLine();
+
+            //bool isb1 = b1 is Book;
+
+            Console.WriteLine("Olika test:");
+            Console.WriteLine("Är b1 en bok?       " + (b1 is Book));
+            Console.WriteLine("Är b1 en e-bok?     " + (b1 is ElectronicBook));
+            Console.WriteLine("Är b1 en product?   " + (b1 is Product));
+
+            Console.WriteLine();
+            var e1 = new ElectronicBook("465745", "Min e-bok");
+            e1.ProductId="7777";
+            Console.WriteLine($"Info om e-boken:");
+            Console.WriteLine($"   ProductId:   {e1.ProductId}");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Olika test:");
+            Console.WriteLine("Är e1 en bok?       " + (e1 is Book));
+            Console.WriteLine("Är e1 en e-bok?     " + (e1 is ElectronicBook));
+            Console.WriteLine("Är e1 en product?   " + (e1 is Product));
+
+            Console.WriteLine();
+            Console.WriteLine("Anrop:");
+            e1.SendBookTo("astrid@gemigboken.nu");
+        }
+    }
+}
