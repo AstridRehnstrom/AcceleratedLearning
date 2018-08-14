be3adc8d36cef3b34dc66a5796194223bcbfed28
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-14 08:43:48 +0200

    :)

---
+++ 7.1 Setup classes/7.1 Setup classes/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Setup_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var triangle = new Triangle();
+            //var circle = new Circle();
+            //var rectangle = new Rectangle();
+            //var listOfShapes = new List<Shape>();
+            //Console.WriteLine("Select (T)riangle, (R)ectangle, (C)ircle, or (D)one");
+            //string inmatat = Console.ReadLine().ToUpper();
+            List<Shape> shapeList = listOfShapes();
+
+            foreach (Shape shapes in shapeList)
+            {
+
+                Console.WriteLine(shapes.ToString());
+            }
+        }
+
+        public static List<Shape> listOfShapes() {
+                bool oneMore = true;
+                List<Shape> allShapes = new List<Shape>();
+                do
+                {
+                Console.WriteLine("Select (T)riangle, (R)ectangle, (C)ircle, or (D)one");
+                string inmatat = Console.ReadLine().ToUpper();
+
+                switch (inmatat)
+                    {
+                        case "C":
+                            {
+                                Circle c = AskForCircle();
+                                allShapes.Add(c);
+                                break;
+                            }
+
+                        case "R":
+                            {
+                                Rectangle r = AskForRectangle();
+                                allShapes.Add(r);
+                                break;
+                            }
+
+                        case "T":
+                            {
+
+                                Triangle t = AskForTriangle();
+                                allShapes.Add(t);
+                                break;
+                            }
+
+                        case "D":
+                            oneMore = false;
+                            break;
+                    }
+                } while (oneMore);
+                return allShapes;
+            }
+
+            //foreach (Shape shapes in listOfShapes)
+            //{
+            //    Console.WriteLine(shapes.ToString());
+            //}
+
+
+        
+
+        public static Circle AskForCircle()
+        {
+
+            Console.WriteLine("Enter radius of circle:");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            var c = new Circle();
+            c.radius = radius;
+            return c;
+
+        }
+
+        public static Rectangle AskForRectangle()
+        {
+            Console.WriteLine("Enter base of rectangle:");
+            int bas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height of rectangle:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            var r = new Rectangle();
+            r.bas = bas;
+            r.height = height;
+            return r;
+
+
+        }
+
+        public static Triangle AskForTriangle()
+        {
+            Console.WriteLine("Enter base of triangle:");
+            int bas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height of triangle:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            var t = new Triangle();
+            t.bas = bas;
+            t.height = height;
+            return t;
+
+        }
+    }
+}
+
---
+++ 7.1 Setup classes/7.1 Setup classes/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Setup_classes
+{
+    class Shape
+    {
+       
+    }
+
+    class Triangle : Shape
+    {
+        public int bas { get; set; }
+        public int height { get; set; }
+    }
+
+    class Circle : Shape
+    {
+        public int radius { get; set; }
+    }
+
+
+    class Rectangle : Shape
+    {
+        public int bas { get; set; }
+        public int height { get; set; }
+    }
+}
