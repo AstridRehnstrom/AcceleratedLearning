502bd3d0b1fea9864f1b727e2564aa6a8dde4e3a
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-15 15:13:07 +0200

    :)

---
+++ 9.2 Watch a folder/9.2 Watch a folder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace _9._2_Watch_a_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new FileSystemWatcher();
+
+            x.Path = @"c:\TMP";
+            Console.WriteLine("I'm watching this folder " + x.Path);
+        
+            x.EnableRaisingEvents = true;
+
+            x.Created += EnFilHarLagtsTill;
+            x.Deleted += EnFilHarTagitsBort;
+            x.Changed += EnFilHarÄndrats;
+
+            Console.ReadKey();
+           
+        }
+
+        private static void EnFilHarÄndrats(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Filen " + e.Name + " har ändrats!");
+        }
+
+        private static void EnFilHarTagitsBort(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Filen " + e.Name + " har tagits bort!");
+        }
+
+        private static void EnFilHarLagtsTill(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Filen " + e.Name + " har skapats!");
+        }
+
+     
+
+
+    }
+
+
+}
