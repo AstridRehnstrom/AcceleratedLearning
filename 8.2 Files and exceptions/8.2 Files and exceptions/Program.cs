9ce3a68974b4daf15462444f39045603ac78c37a
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-15 11:03:11 +0200

    :)

---
+++ 8.2 Files and exceptions/8.2 Files and exceptions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace _8._2_Files_and_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            while (true)
+            {
+                Console.Write("Enter a file name: ");
+
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    var filename = Console.ReadLine();
+                    Console.ResetColor();
+                    File.CreateText(filename);
+                    Console.WriteLine("The file " + filename + " is now created.");
+                    break;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You aren’t allowed to create the file here");
+                }
+
+                catch (ArgumentException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid filename");
+                    Console.ResetColor();
+                }
+
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The folder don’t exist");
+                    Console.ResetColor();
+                }
+
+            }
+
+
+        }
+    }
+}
