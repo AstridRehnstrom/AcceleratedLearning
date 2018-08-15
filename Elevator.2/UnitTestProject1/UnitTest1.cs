aa77c05219139ec51c4b8e9ffb9ebd2dcbbb84c4
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-15 09:07:53 +0200

    :)

---
+++ Elevator.2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using ElevatorDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var e1= new Elevator()
+
+        }
+    }
+}
