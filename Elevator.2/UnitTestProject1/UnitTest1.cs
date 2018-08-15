df8c3102411b02d99dfda10be50feddf076a714e
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-15 16:51:25 +0200

    :)

--- Elevator.2/UnitTestProject1/UnitTest1.cs
+++ Elevator.2/UnitTestProject1/UnitTest1.cs
@@ -6,10 +6,27 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        const int bottomfloor = -2;
+        const int topfloor = 3;
+        private const int untilmaintainance = 6;
+
+        Elevator e1 = new Elevator("Kalle", bottomfloor, topfloor,  1,  6);
+
         [TestMethod]
-        public void TestMethod1()
+        public void elevator_should_be_at_level_2_when_it_starts_at_1_and_go_up_one_floor()
         {
-            var e1= new Elevator()
+            e1.GoUp();
+            Assert.AreEqual(2, e1.CurrentFloor);
+
+        }
+
+
+        [TestMethod]
+        public void elevaror_should_be_at_level_3_when_it_starts_at_1_and_go_up_is_called_twice()
+        {
+            e1.GoUp();
+            e1.GoUp();
+            Assert.AreEqual(3, e1.CurrentFloor);
 
         }
     }
