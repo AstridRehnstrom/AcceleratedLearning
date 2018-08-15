a53ae87cbc94d7217e4aa684ddd32a119ae43dc5
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-15 10:12:22 +0200

    :)

--- 8.1 Dividing chocolate/8.1 Dividing chocolate/Program.cs
+++ 8.1 Dividing chocolate/8.1 Dividing chocolate/Program.cs
@@ -7,29 +7,46 @@ namespace _8._1_Dividing_chocolate
         static void Main(string[] args)
         {
             Console.WriteLine("The chocolate contains 24 pieces");
-            Console.Write("How many want to share? ");
-            Console.ForegroundColor = ConsoleColor.Green;
-
-
-
-
-            try
-            {
-                
-                decimal tal = decimal.Parse(Console.ReadLine());
-                decimal del = 24 / tal;
-                Console.ResetColor();
-                Console.WriteLine("Everyone gets " + Math.Round(del, 2) + " pieces");
-            }
-            catch (Exception)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Zero people can't divide a chocolate.");
-                Console.ResetColor();
-            }
-
+           
             
+            int retries = 0;
+
+            while (true)
+            {
+
+                try
+                {
+                    Console.Write("How many want to share? ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    decimal tal = decimal.Parse(Console.ReadLine());
+                    decimal del = 24M / tal;
+                    Console.ResetColor();
+                    Console.WriteLine("Everyone gets " + Math.Round(del, 2) + " pieces");
+                    break;
+                }
+
+                catch (FormatException)
+                {
+                    Console.ResetColor();
+                    Console.WriteLine("Det är fel på formatet");
+                    if (retries < 3)
+                        retries++;
+                    else
+                        throw;
+                }
+
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zero people can't divide a chocolate.");
+                    Console.ResetColor();
+                    if (retries < 3)
+                        retries++;
+                    else
+                        throw;
+                }
+
+            }
         }
     }
 }
- 
