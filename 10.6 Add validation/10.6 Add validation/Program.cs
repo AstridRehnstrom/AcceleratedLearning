add155bc7329f585587084e84657944c47344cbe
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-16 11:55:19 +0200

    :)

--- 10.6 Add validation/10.6 Add validation/Program.cs
+++ 10.6 Add validation/10.6 Add validation/Program.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _10._5_Product_list_with_dictionary
+namespace _10._6_Add_validation
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Dictionary<int, string> products = BuildProductDictionary();
             DisplayProductDictionary(products);
-          
         }
+
+
 
 
         private static void DisplayProductDictionary(Dictionary<int, string> dic)
         {
             foreach (var item in dic)
             {
-                Console.WriteLine("Product ID = "+ item.Key + " and name: " + item.Value);
-              
+                Console.WriteLine("Product ID = " + item.Key + " and name: " + item.Value);
+
             }
         }
 
@@ -30,7 +32,6 @@ namespace _10._5_Product_list_with_dictionary
             while (true)
             {
 
-
                 Console.WriteLine("Enter Product ID and name (separated by comma), press enter to quit");
                 string inmatat = Console.ReadLine();
 
@@ -39,8 +40,27 @@ namespace _10._5_Product_list_with_dictionary
 
                 string[] products = inmatat.Split(',');
 
-                int id = Convert.ToInt32(products[0]);
-                string name = products[1];
+                int id;
+                string name;
+
+                try
+                {
+                    id = Convert.ToInt32(products[0]);
+                    name = products[1];
+                }
+
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                catch (System.IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+              
 
                 if (dic.ContainsKey(id))
                 {
@@ -51,8 +71,10 @@ namespace _10._5_Product_list_with_dictionary
                 dic.Add(id, name);
 
 
+
             }
             return dic;
         }
     }
+
 }
