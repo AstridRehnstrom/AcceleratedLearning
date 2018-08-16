3472906328ed3ef22053ecbe2b6a157f4b695a56
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-16 16:31:34 +0200

    :)

---
+++ 11.2 Work with list of Customers/11.2 Work with list of Customers/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _11._2_Work_with_list_of_Customers
+{
+    public enum Gender
+    {
+        Female, Male, Other
+    }
+
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var allLines = File.ReadAllLines("People.txt");
+            var allCustomers = new List<Customer>();
+
+
+            foreach (var line in allLines)
+            {
+
+                var splittedLine = line.Split(',');
+                var id = splittedLine[0];
+                var name = splittedLine[1];
+                var lastName = splittedLine[2];
+                var email = splittedLine[3];
+                var gender = splittedLine[4];
+                var age = splittedLine[5];
+
+                var customer = new Customer();
+
+                //var xx = Enum.Parse<Gender>("aaaa");
+                customer.Id = int.Parse(id);
+                customer.Name = name;
+                customer.LastName = lastName;
+                customer.Email = email;
+                customer.Age = int.Parse(age);
+                customer.Gender = Enum.Parse<Gender>(gender);
+
+                allCustomers.Add(customer);
+            }
+
+            Console.WriteLine("Sorted list by age:");
+
+            var sortedByAge = allCustomers.OrderBy(x => x.Age);
+
+            foreach (var item in sortedByAge)
+            {
+                Console.WriteLine(item.Name.PadRight(10) + "    " + item.Age.ToString().PadRight(4) + " " + item.Gender);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted list by name:");
+            var sortedByName = allCustomers.OrderBy(x => x.Name);
+
+            foreach (var item in sortedByName)
+            {
+                Console.WriteLine(item.Name.PadRight(10) + "    " + item.Age.ToString().PadRight(4) + " " + item.Gender);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Men Older than 35:");
+            var menOlder= allCustomers.Where(x => x.Gender==Gender.Male && x.Age >35).OrderBy(x => x.Name);
+
+            foreach (var item in menOlder)
+            {
+                Console.WriteLine(item.Name.PadRight(10) + "    " + item.Age.ToString().PadRight(4) + " " + item.Gender);
+            }
+
+        }
+
+    }
+}
+
---
+++ TV-tablå/TV-tablå/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TV_tablå
+{
+   
+    class Show
+    {
+        public string Channel { get; set; }
+        public TimeSpan StartAt { get; set; }
+        public string Title { get; set; }
+    }
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var allLines= File.ReadAllLines("TV-data.txt");
+
+            var allShows = new List<Show>();
+
+            foreach (var line in allLines)
+            {
+
+                var splitteLine= line.Split('*');
+                var channel = splitteLine[0];
+                var time = splitteLine[1];
+                var title = splitteLine[2];
+
+                var show = new Show();
+
+                show.Title = title;
+                show.Channel = channel;
+                show.StartAt = TimeSpan.Parse(time);
+
+                allShows.Add(show);
+            }
+            
+
+            Header("alla shower");
+
+            foreach (var show in allShows)
+            {
+                Console.WriteLine(show.Title);
+            }
+
+            Header("alla kanaler");
+            
+            foreach (var show in allShows)
+            {
+                Console.WriteLine(show.Channel);
+            }
+
+
+            var laterThan21 = new List<Show>();
+
+            foreach (var show in allShows)
+            {
+                if (show.StartAt.Hours >= 21)
+                {
+                    laterThan21.Add(show);
+                }                
+            }
+
+          
+            Header("Program som börjar senare än 21:");
+            foreach (var show in laterThan21)
+            {
+                WriteInfoAboutShow(show);            
+            }
+
+
+            Header("Program som börjar senare än 21, mha linq:");
+
+            var resultat = allShows.Where(show => show.StartAt.Hours >= 21);
+
+            foreach (var show in resultat)
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            Header("Program på svt1 sorterade i kronologisk ordning");
+            var svt1sort= allShows.Where(x => x.Channel == "SVT1").OrderBy(x => x.StartAt);
+
+            foreach (var show in svt1sort)
+            {
+                WriteInfoAboutShow(show);
+            }
+
+        }
+
+
+        private static void Header(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n" + text.ToUpper() + "\n");
+            Console.ResetColor();
+        }
+
+
+        private static void WriteInfoAboutShow(Show show)
+        {
+            Console.WriteLine(show.Channel.PadRight(4) + "    " + show.StartAt + " " + show.Title);
+        }
+
+       
+    }
+    
+}
