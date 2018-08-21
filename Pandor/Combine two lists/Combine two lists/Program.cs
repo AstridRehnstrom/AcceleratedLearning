fe4746820f66c9fc1001a8e16c2aefd4be786b28
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-21 14:12:43 +0200

    :)

---
+++ Pandor/Combine two lists/Combine two lists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combine_two_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista1 = new List<string> { "a", "b", "c"};
+            List<int> lista2 = new List<int> { 1, 2, 3 };
+
+            List<string> listaTotal = new List<string>();
+
+            for (int i = 0; i < lista1.Count; i++)
+            {
+                listaTotal.Add(lista1[i]);
+                listaTotal.Add(lista2[i].ToString());
+
+            }
+
+
+            for (int i = 0; i < listaTotal.Count; i++)
+            {
+                if (i < listaTotal.Count - 1)
+                {
+                    Console.Write($"{listaTotal[i]},");
+                }
+                else
+                {
+                    Console.Write($"{listaTotal[i]}");
+                }
+
+            }
+            Console.WriteLine();
+
+
+
+
+            //foreach (var item in listaTotal)
+            //{
+            //    Console.Write($" {item},");
+            //}
+
+
+        }
+    }
+}
---
+++ Pandor/Rotation/Rotation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int> {1,2,3,4,5};
+
+
+
+
+        }
+    }
+}
