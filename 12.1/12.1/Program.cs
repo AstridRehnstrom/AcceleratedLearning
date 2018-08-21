745a9e946d4028cc9e013c164f9db40cb8aff22e
Astrid Rehnström <astrid.rehnstrom@gmail.com>
2018-08-21 08:24:57 +0200

    :)

---
+++ 12.1/12.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] rockstarsArray = new string[] { "Jim Morrison", "Ozzy Osborne", "David Bowie", "Freddie Mercury"};
+            List<string> rockstarsList = new List<string> { "Jim Morrison", "Ozzy Osborne", "David Bowie", "Freddie Mercury" };
+            Console.WriteLine("RockstarsList, med metod för array");
+            DisplayArrayOfRockStars(rockstarsArray);
+            Console.WriteLine();
+            Console.WriteLine("RockstarsList, med metod för lista");
+            DisplayListOfRockStars(rockstarsList);
+            Console.WriteLine();
+            Console.WriteLine("RockstarsList, med metod för IEnumerable, input array");
+            DisplayRockStars(rockstarsArray);
+            Console.WriteLine();
+            Console.WriteLine("RockstarsList, med metod för IEnumerable, input lista");
+            DisplayRockStars(rockstarsList);
+            string nyttnamn = "Zara Larsson";
+            //rockstarsArray.Add(nyttnamn); -går ej
+            rockstarsList.Add(nyttnamn);
+            
+        }
+
+        static void DisplayArrayOfRockStars(string[] rockstarsArray)
+        {
+            
+            foreach (var name in rockstarsArray)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+        static void DisplayListOfRockStars(List<string> rockstars)
+        {
+            foreach (var name in rockstars) 
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void DisplayRockStars(IEnumerable<string> rockstars)
+        {
+            foreach (var name in rockstars)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
---
+++ 12.2 Custom interfaces/12.2 Custom interfaces/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace _12._2_Custom_interfaces
+{
+    public interface IFriendly
+    {
+        void Greet();
+        void ThanksForDinner(string meal);
+    }
+
+    public interface IAggressive
+    {
+        int Bite();
+        void ShowTeeth();
+    }
+
+    class Dog : IFriendly, IAggressive
+    {
+        public Dog(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public int Bite()
+        {
+            Console.WriteLine("The dog " + Name + " bites you");
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("The dog " + Name + " says woff");
+        }
+
+        public void ShowTeeth()
+        {
+            Console.WriteLine("The dog "+ Name + " show his teeth");
+        }
+
+        public void ThanksForDinner(string meal)
+        {
+            Console.WriteLine("The Dog "+ Name + " thank you for the " + meal);
+        }
+    }
+    class Cat : IFriendly
+    {
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+
+        public void Greet()
+        {
+            Console.WriteLine("The cat " + Name + " says miao");
+        }
+
+        public void ThanksForDinner(string meal)
+        {
+            Console.WriteLine("The cat " + Name + " thank you for the " + meal);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog buddy = new Dog("Buddy");
+            Cat musse = new Cat("Musse");
+
+            DoNiceThings(buddy);
+            DoMeanThings(buddy);
+
+            DoNiceThings(musse);
+        }
+
+        private static void DoMeanThings(IAggressive a)
+        {
+            a.Bite();
+            a.ShowTeeth();
+        }
+
+
+        private static void DoNiceThings(IFriendly a)
+        {
+            a.Greet();
+            a.ThanksForDinner("Fish");
+                
+
+        }
+    }
+
+}
+
